94615446be8223f4ebdda131f3902d64ed2d9da3
Robert Deivson <robert@pop-os.localdomain>
2021-10-22 12:58:09 -0300

    finalizando as classes de dominio

--- src/main/java/com/robert/bookstore/domain/Livro.java
+++ src/main/java/com/robert/bookstore/domain/Livro.java
@@ -1,9 +1,20 @@
 package com.robert.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Livro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
