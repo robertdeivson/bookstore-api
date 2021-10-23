7f517e7de173f1324a2d935eebf161f3572b8400
Robert Deivson <robert@pop-os.localdomain>
2021-10-23 19:41:36 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/robert/bookstore/service/CategoriaService.java
+++ src/main/java/com/robert/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.robert.bookstore.domain.Categoria;
 import com.robert.bookstore.repository.CategoriaRepository;
+import com.robert.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo:" + Categoria.class.getName()));
 	}
 }
