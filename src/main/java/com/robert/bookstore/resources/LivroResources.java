9131083af5f9cff9fbc5f190a4573f47df143459
Robert Deivson <robert@pop-os.localdomain>
2021-10-24 16:50:17 -0300

    Listando todos os livros por categoria

--- src/main/java/com/robert/bookstore/resources/LivroResources.java
+++ src/main/java/com/robert/bookstore/resources/LivroResources.java
@@ -1,12 +1,18 @@
 package com.robert.bookstore.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.robert.bookstore.DTO.LivroDTO;
 import com.robert.bookstore.domain.Livro;
 import com.robert.bookstore.service.LivroService;
 
@@ -23,5 +29,11 @@ public class LivroResources {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDto = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 	
 }
